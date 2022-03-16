cc101c58924b166952badd5bed721757ea6f822a
Timotej Bojarovski <Timotej.Bojarovski@sedc.mk>
2022-03-16 10:42:08 -0700

    git test

---
+++ Sedc.OOP.class5.ClassesAndObjects/Sedc.OOP.class5.ClassedAndObjects.app/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sedc.OOP.class5.ClassedAndObjects.app.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+
+        private long GenerateSSN()
+        {
+            Random RandomSSN = new Random();
+            long ssn = RandomSSN.Next(100_000, 999_999);
+            return ssn;
+        }
+        public void Talk(string text)
+        {
+            Console.WriteLine($"The human name {Name} (SSN: {SSN} is saying: {text}");
+        }
+    }
+}
