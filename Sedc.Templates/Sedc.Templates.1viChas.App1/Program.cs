5987d3aad769e2fda7d934ef22a03c7921e77895
Timotej Bojarovski <Timotej.Bojarovski@sedc.mk>
2022-03-16 10:39:58 -0700

    Git test

---
+++ Sedc.Templates/Sedc.Templates.1viChas.App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sedc.Templates._1viChas.App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
